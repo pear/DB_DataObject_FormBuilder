8b28c1a11985fd07d015b6e0094334d45c1ee7b7
Justin Patrin <justinpatrin@php.net>
2005-02-28 05:21:39 +0000

    Updated docs, added $this as last parameter to getForm
    git-svn-id: https://svn.php.net/repository/pear/packages/DB_DataObject_FormBuilder/trunk@180993 c90b9560-bf6c-de11-be94-00142212c4b1

--- FormBuilder.php
+++ FormBuilder.php
@@ -56,20 +56,20 @@
  *   yet copied the options from the DataObject into it. If you plan to use the
  *   functions in FB in this method, call populateOptions() on it first.
  *  </li>
- *  <li>postGenerateForm(&$form):
+ *  <li>postGenerateForm(&$form, &$formBuilder):
  *   This method will be called after the form is generated. The form is passed in by reference so you can
  *   alter it. Use this method to add, remove, or alter elements in the form or the form itself.
  *  </li>
- *  <li>preProcessForm(&$values):
+ *  <li>preProcessForm(&$values, &$formBuilder):
  *   This method is called just before FormBuilder processes the submitted form data. The values are sent
  *   by reference in the first parameter as an associative array. The key is the element name and the value
  *   the submitted value. You can alter the values as you see fit (md5 on passwords, for example).
  *  </li>
- *  <li>postProcessForm(&$values):
+ *  <li>postProcessForm(&$values, &$formBuilder):
  *   This method is called just after FormBuilder processed the submitted form data. The values are again
  *   sent by reference. This method could be used to inform the user of changes, alter the DataObject, etc.
  *  </li>
- *  <li>getForm():
+ *  <li>getForm($action, $target, $formName, $method, &$formBuilder):
  *   If this function exists, it will be used instead of FormBuilder's internal form generation routines
  *   Use this only if you want to create the entire form on your own.
  *  </li>
@@ -1831,7 +1831,7 @@ class DB_DataObject_FormBuilder
         }
         
         if (method_exists($this->_do, 'getform')) {
-            $obj = $this->_do->getForm($action, $target, $formName, $method);
+            $obj = $this->_do->getForm($action, $target, $formName, $method, &$this);
         } else {
             $obj = &$this->_generateForm($action, $target, $formName, $method);
         }
