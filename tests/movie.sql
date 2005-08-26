806fc6d0ae290e152da0bddd4e6a2f6cf5e3dc32
Justin Patrin <justinpatrin@php.net>
2005-08-26 17:47:18 +0000

    Added a text element test Added a summary field for text element test Updated sql file
    git-svn-id: https://svn.php.net/repository/pear/packages/DB_DataObject_FormBuilder/trunk@194461 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/movie.sql
+++ tests/movie.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 9.11
 --
--- Host: localhost    Database: reversefold
+-- Host: localhost    Database: FormBuilder_testing
 -- ------------------------------------------------------
--- Server version	4.0.24-max-log
+-- Server version	4.0.24
 
 --
 -- Table structure for table `audioFormat`
@@ -18,7 +18,12 @@ CREATE TABLE `audioFormat` (
 -- Dumping data for table `audioFormat`
 --
 
-INSERT INTO `audioFormat` VALUES (1,'Mono'),(2,'Stereo'),(3,'Dolby Pro Logic'),(4,'Dolby Digital 5.1'),(5,'Dolby Digital EX'),(6,'DTS');
+INSERT INTO `audioFormat` VALUES (1,'Mono');
+INSERT INTO `audioFormat` VALUES (2,'Stereo');
+INSERT INTO `audioFormat` VALUES (3,'Dolby Pro Logic');
+INSERT INTO `audioFormat` VALUES (4,'Dolby Digital 5.1');
+INSERT INTO `audioFormat` VALUES (5,'Dolby Digital EX');
+INSERT INTO `audioFormat` VALUES (6,'DTS');
 
 --
 -- Table structure for table `genre`
@@ -34,7 +39,12 @@ CREATE TABLE `genre` (
 -- Dumping data for table `genre`
 --
 
-INSERT INTO `genre` VALUES (1,'Sci-Fi'),(2,'Fantasy'),(3,'Romance'),(4,'Action'),(5,'Horror'),(13,'Alien');
+INSERT INTO `genre` VALUES (1,'Sci-Fi');
+INSERT INTO `genre` VALUES (2,'Fantasy');
+INSERT INTO `genre` VALUES (3,'Romance');
+INSERT INTO `genre` VALUES (4,'Action');
+INSERT INTO `genre` VALUES (5,'Horror');
+INSERT INTO `genre` VALUES (13,'Alien');
 
 --
 -- Table structure for table `language`
@@ -50,7 +60,55 @@ CREATE TABLE `language` (
 -- Dumping data for table `language`
 --
 
-INSERT INTO `language` VALUES (1,'English'),(2,'German'),(3,'French'),(4,'Spanish'),(5,'Japanese'),(6,'Chinese');
+INSERT INTO `language` VALUES (1,'English');
+INSERT INTO `language` VALUES (2,'German');
+INSERT INTO `language` VALUES (3,'French');
+INSERT INTO `language` VALUES (4,'Spanish');
+INSERT INTO `language` VALUES (5,'Japanese');
+INSERT INTO `language` VALUES (6,'Chinese');
+
+--
+-- Table structure for table `linkTest`
+--
+
+CREATE TABLE `linkTest` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(10) default NULL,
+  `name2` varchar(15) default NULL,
+  `linkTest2_id` int(10) unsigned default NULL,
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM;
+
+--
+-- Dumping data for table `linkTest`
+--
+
+INSERT INTO `linkTest` VALUES (1,'1name','2name',1);
+INSERT INTO `linkTest` VALUES (2,'test 2','test 2 2',1);
+INSERT INTO `linkTest` VALUES (3,'test 3','test 3 2',2);
+INSERT INTO `linkTest` VALUES (4,'test','testt',NULL);
+INSERT INTO `linkTest` VALUES (5,'another','yep',2);
+INSERT INTO `linkTest` VALUES (6,'test','2 test',NULL);
+
+--
+-- Table structure for table `linkTest2`
+--
+
+CREATE TABLE `linkTest2` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(10) default NULL,
+  `name2` varchar(15) default NULL,
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM;
+
+--
+-- Dumping data for table `linkTest2`
+--
+
+INSERT INTO `linkTest2` VALUES (1,'1 name','2 name');
+INSERT INTO `linkTest2` VALUES (2,'blah','foo');
+INSERT INTO `linkTest2` VALUES (3,'blah','abc');
+INSERT INTO `linkTest2` VALUES (4,'straw','abc');
 
 --
 -- Table structure for table `movie`
@@ -64,6 +122,10 @@ CREATE TABLE `movie` (
   `enumTest` enum('option 1','option 2','option 3') default NULL,
   `enumTest2` enum('option a','option b','optionc3') default NULL,
   `anotherField` varchar(20) default NULL,
+  `timeField` time default NULL,
+  `dateField` date default NULL,
+  `linkTest_id` int(10) unsigned default NULL,
+  `summary` text,
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
 
@@ -71,7 +133,14 @@ CREATE TABLE `movie` (
 -- Dumping data for table `movie`
 --
 
-INSERT INTO `movie` VALUES (4,'Alien',5,'2005-05-24 22:41:51','option 2','optionc3',''),(15,'Alien Resurrection',4,'2005-06-06 23:08:37','option 2','',''),(13,'Aliens',4,'2005-06-06 23:08:23','option 2','',''),(14,'Alien3',4,'2005-06-06 23:08:32','option 3','',''),(16,'Brazil',4,'2005-06-06 23:08:44','option 1','',''),(17,'Shawn of the Dead',5,'2005-06-06 23:08:55','option 1','',''),(18,'12 Monkeys',4,'2005-06-06 23:09:01','option 1','',''),(19,'The Ring',5,'2005-06-23 02:39:53','option 3','optionc3','');
+INSERT INTO `movie` VALUES (4,'Alien',5,'2005-05-24 22:41:51','option 2','optionc3','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (15,'Alien Resurrection',4,'2005-06-06 23:08:37','option 2','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (13,'Aliens',4,'2005-06-06 23:08:23','option 2','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (14,'Alien3',4,'2005-06-06 23:08:32','option 3','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (16,'Brazil',4,'2005-06-06 23:08:44','option 1','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (17,'Shawn of the Dead',5,'2005-06-06 23:08:55','option 1','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (18,'12 Monkeys',4,'2005-06-06 23:09:01','option 1','','',NULL,NULL,1,NULL);
+INSERT INTO `movie` VALUES (19,'The Ring',5,'2005-06-23 02:39:53','option 3','optionc3','',NULL,NULL,1,NULL);
 
 --
 -- Table structure for table `movie_audioFormat_language`
@@ -89,7 +158,17 @@ CREATE TABLE `movie_audioFormat_language` (
 -- Dumping data for table `movie_audioFormat_language`
 --
 
-INSERT INTO `movie_audioFormat_language` VALUES (35,17,1,5),(34,17,6,3),(33,17,3,2),(32,17,3,1),(31,17,4,1),(25,4,1,5),(26,4,3,1),(16,4,4,1),(23,4,3,5),(36,19,5,3),(37,19,3,6);
+INSERT INTO `movie_audioFormat_language` VALUES (35,17,1,5);
+INSERT INTO `movie_audioFormat_language` VALUES (34,17,6,3);
+INSERT INTO `movie_audioFormat_language` VALUES (33,17,3,2);
+INSERT INTO `movie_audioFormat_language` VALUES (32,17,3,1);
+INSERT INTO `movie_audioFormat_language` VALUES (31,17,4,1);
+INSERT INTO `movie_audioFormat_language` VALUES (25,4,1,5);
+INSERT INTO `movie_audioFormat_language` VALUES (26,4,3,1);
+INSERT INTO `movie_audioFormat_language` VALUES (16,4,4,1);
+INSERT INTO `movie_audioFormat_language` VALUES (23,4,3,5);
+INSERT INTO `movie_audioFormat_language` VALUES (36,19,5,3);
+INSERT INTO `movie_audioFormat_language` VALUES (37,19,3,6);
 
 --
 -- Table structure for table `movie_song`
@@ -107,7 +186,25 @@ CREATE TABLE `movie_song` (
 -- Dumping data for table `movie_song`
 --
 
-INSERT INTO `movie_song` VALUES (33,18,2,2),(32,17,4,2),(24,4,5,1),(34,16,1,1),(26,4,1,NULL),(31,17,1,1),(35,16,3,2),(36,15,5,2),(37,15,4,2),(38,15,3,2),(39,13,2,1),(40,13,5,1),(41,13,1,2),(42,14,2,2),(43,14,1,NULL),(44,14,3,1),(45,18,5,NULL),(46,18,1,NULL),(47,18,4,NULL);
+INSERT INTO `movie_song` VALUES (33,18,2,2);
+INSERT INTO `movie_song` VALUES (32,17,4,2);
+INSERT INTO `movie_song` VALUES (24,4,5,1);
+INSERT INTO `movie_song` VALUES (34,16,1,1);
+INSERT INTO `movie_song` VALUES (26,4,1,NULL);
+INSERT INTO `movie_song` VALUES (31,17,1,1);
+INSERT INTO `movie_song` VALUES (35,16,3,2);
+INSERT INTO `movie_song` VALUES (36,15,5,2);
+INSERT INTO `movie_song` VALUES (37,15,4,2);
+INSERT INTO `movie_song` VALUES (38,15,3,2);
+INSERT INTO `movie_song` VALUES (39,13,2,1);
+INSERT INTO `movie_song` VALUES (40,13,5,1);
+INSERT INTO `movie_song` VALUES (41,13,1,2);
+INSERT INTO `movie_song` VALUES (42,14,2,2);
+INSERT INTO `movie_song` VALUES (43,14,1,NULL);
+INSERT INTO `movie_song` VALUES (44,14,3,1);
+INSERT INTO `movie_song` VALUES (45,18,5,NULL);
+INSERT INTO `movie_song` VALUES (46,18,1,NULL);
+INSERT INTO `movie_song` VALUES (47,18,4,NULL);
 
 --
 -- Table structure for table `movie_song__type`
@@ -123,7 +220,8 @@ CREATE TABLE `movie_song__type` (
 -- Dumping data for table `movie_song__type`
 --
 
-INSERT INTO `movie_song__type` VALUES (1,'Inspired By'),(2,'Soundtrack');
+INSERT INTO `movie_song__type` VALUES (1,'Inspired By');
+INSERT INTO `movie_song__type` VALUES (2,'Soundtrack');
 
 --
 -- Table structure for table `song`
@@ -139,5 +237,9 @@ CREATE TABLE `song` (
 -- Dumping data for table `song`
 --
 
-INSERT INTO `song` VALUES (1,'Forty-Six and Two'),(2,'A Change of Seasons'),(3,'Watermark'),(4,'Gnosiennes'),(5,'Absolution');
+INSERT INTO `song` VALUES (1,'Forty-Six and Two');
+INSERT INTO `song` VALUES (2,'A Change of Seasons');
+INSERT INTO `song` VALUES (3,'Watermark');
+INSERT INTO `song` VALUES (4,'Gnosiennes');
+INSERT INTO `song` VALUES (5,'Absolution');
 
---
+++ tests/textElement.phpt
@@ -0,0 +1,15 @@
+--TEST--
+text elements
+--FILE--
+<?php
+include(dirname(__FILE__).'/config.php');
+$do =& DB_DataObject::factory('movie');
+$fb =& DB_DataObject_FormBuilder::create($do);
+$form =& $fb->getForm();
+$el =& $form->getElement('summary');
+var_dump($el->_type);
+var_dump(strtolower(get_class($el)));
+?>
+--EXPECT--
+string(8) "textarea"
+string(23) "html_quickform_textarea"
