da136c936b4be0416380bebbd54fbb5fc7c831db
Justin Patrin <justinpatrin@php.net>
2005-10-13 04:59:26 +0000

    Attempt to fix the subForm file element enctype issue more simply
    git-svn-id: https://svn.php.net/repository/pear/packages/DB_DataObject_FormBuilder/trunk@198322 c90b9560-bf6c-de11-be94-00142212c4b1

--- FormBuilder/QuickForm/SubForm.php
+++ FormBuilder/QuickForm/SubForm.php
@@ -108,7 +108,7 @@ class HTML_QuickForm_SubForm extends HTML_QuickForm_static {
     function setForm(&$form)
     {
         $this->_subForm =& $form;
-        $this->_checkForFileElement();
+        $this->_checkForEnctype();
     }
 
 
@@ -171,7 +171,7 @@ class HTML_QuickForm_SubForm extends HTML_QuickForm_static {
         $this->_parentForm =& $form;
         $this->_parentForm->addFormRule(array(&$this, 'checkSubFormRules'));
         $this->_ruleRegistered = true;
-        $this->_checkForFileElement();
+        $this->_checkForEnctype();
     }
 
     /**
@@ -242,14 +242,18 @@ class HTML_QuickForm_SubForm extends HTML_QuickForm_static {
         return true;
     }
 
-    function _checkForFileElement() {
+    function _checkForEnctype() {
         if ($this->_subForm && $this->_parentForm) {
-            foreach (array_keys($this->_subForm->_elements) as $key) {
+            if ($this->_subForm->getAttribute('enctype') == 'multipart/form-data') {
+                $this->_parentForm->updateAttributes(array('enctype' => 'multipart/form-data'));
+                $this->_parentForm->setMaxFileSize();
+            }
+            /*foreach (array_keys($this->_subForm->_elements) as $key) {
                 if (is_a($this->_subForm->_elements[$key], 'HTML_QuickForm_file')) {
                     $this->_parentForm->updateAttributes(array('enctype' => 'multipart/form-data'));
                     $this->_parentForm->setMaxFileSize();
                 }
-            }
+            }*/
         }
     }
 }
