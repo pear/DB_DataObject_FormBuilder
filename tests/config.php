14461ee4cddeb8ed4424ffa580e4c0fac3af360a
Daniel Convissor <danielc@analysisandsolutions.com>
2012-01-09 13:45:36 -0500

    Use environment variables for test DSN.

--- tests/config.php
+++ tests/config.php
@@ -1,17 +1,65 @@
 <?php
-ini_set('include_path', '.'.PATH_SEPARATOR.
-        dirname(__FILE__).PATH_SEPARATOR.
-        '/home/papercrane/include'.PATH_SEPARATOR.
-        ini_get('include_path'));
 require_once 'DB/DataObject.php';
 require_once 'DB/DataObject/FormBuilder.php';
-if (!file_exists(dirname(__FILE__).'/DSN')) {
-    echo 'skip You need to put your DB DSN in the file "DSN" and import movie.sql before running these tests';
+
+if (!empty($_ENV['MYSQL_TEST_USER']) && extension_loaded('mysqli')) {
+    $dsn = array(
+        'phptype' => 'mysqli',
+        'username' => $_ENV['MYSQL_TEST_USER'],
+        'password' => $_ENV['MYSQL_TEST_PASSWD'],
+        'database' => $_ENV['MYSQL_TEST_DB'],
+
+        'hostspec' => empty($_ENV['MYSQL_TEST_HOST'])
+                ? null : $_ENV['MYSQL_TEST_HOST'],
+
+        'port' => empty($_ENV['MYSQL_TEST_PORT'])
+                ? null : $_ENV['MYSQL_TEST_PORT'],
+
+        'socket' => empty($_ENV['MYSQL_TEST_SOCKET'])
+                ? null : $_ENV['MYSQL_TEST_SOCKET'],
+    );
+} elseif (!empty($_ENV['PGSQL_TEST_USER']) && extension_loaded('pgsql')) {
+    $dsn = array(
+        'phptype' => 'pgsql',
+        'username' => $_ENV['PGSQL_TEST_USER'],
+        'password' => $_ENV['PGSQL_TEST_PASSWD'],
+        'database' => $_ENV['PGSQL_TEST_DB'],
+
+        'hostspec' => empty($_ENV['PGSQL_TEST_HOST'])
+                ? null : $_ENV['PGSQL_TEST_HOST'],
+
+        'port' => empty($_ENV['PGSQL_TEST_PORT'])
+                ? null : $_ENV['PGSQL_TEST_PORT'],
+
+        'socket' => empty($_ENV['PGSQL_TEST_SOCKET'])
+                ? null : $_ENV['PGSQL_TEST_SOCKET'],
+
+        'protocol' => empty($_ENV['PGSQL_TEST_PROTOCOL'])
+                ? null : $_ENV['PGSQL_TEST_PROTOCOL'],
+
+        'option' => empty($_ENV['PGSQL_TEST_OPTIONS'])
+                ? null : $_ENV['PGSQL_TEST_OPTIONS'],
+
+        'tty' => empty($_ENV['PGSQL_TEST_TTY'])
+                ? null : $_ENV['PGSQL_TEST_TTY'],
+
+        'connect_timeout' => empty($_ENV['PGSQL_TEST_CONNECT_TIMEOUT'])
+                ? null : $_ENV['PGSQL_TEST_CONNECT_TIMEOUT'],
+
+        'sslmode' => empty($_ENV['PGSQL_TEST_SSL_MODE'])
+                ? null : $_ENV['PGSQL_TEST_SSL_MODE'],
+
+        'service' => empty($_ENV['PGSQL_TEST_SERVICE'])
+                ? null : $_ENV['PGSQL_TEST_SERVICE'],
+    );
+} else {
+    die("skip DSN information not provided\n");
 }
+
 $config = array (
   'DB_DataObject' =>
   array (
-         'database' => file_get_contents(dirname(__FILE__).'/DSN'),
+         'database' => $dsn,
          'schema_location' => dirname(__FILE__).'/DataObjects',
          'class_location' => dirname(__FILE__).'/DataObjects',
          'require_prefix' => 'DataObjects/',
